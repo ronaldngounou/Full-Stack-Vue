c1c4b9ce066ac9a585835f33fa241d96b1ebca6f
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-06-02 17:02:01 -0700

    Create reusable component for the product page

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,26 +1,37 @@
 <template>
-   <div class = "img-wrap">
-        <img :src="product.imageName" />
-   </div>
-
-   <div class = "product-details">
-        <h1>{{ product.name }}</h1>
-        <h3 class = "price"> {{ product.price }} </h3>
-        <button class = "add-to-cart">Add to cart</button>
-   </div>
+    <div class="grid-wrap">
+        <div v-if="product">
+            <div class = "img-wrap">
+                 <img :src="product.imageName" />
+            </div>
+         
+            <div class = "product-details">
+                 <h1>{{ product.name }}</h1>
+                 <h3 class = "price"> {{ product.price }} </h3>
+                 <button class = "add-to-cart">Add to cart</button>
+            </div>
+        </div>
+    
+        <div v-else>
+            <NotFoundPage />
+        </div>
+    </div>
 
 </template>
 
 
 <script>
     import {products} from "@/temp-data.js";
-
+    import NotFoundPage from "./NotFoundView.vue";
     export default {
         name: 'ProductDetailView',
         data(){
             return {
                 product: products.find(product => product.it === this.$route.params.id)
             }
+        },
+        components: {
+            NotFoundPage,
         }
     }
 </script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,30 +1,25 @@
 <template>
     <h1>Products</h1>
-    <div class="grid-wrap">
-        <div class = "product-item"
-            v-for="product in products"
-            :key = "product.id">
-            <img :src="product.imageName" />
-            <h3 class = "product-name">{{ product.name }}</h3>
-            <p class = "product-price">{{ product.price }}</p>
 
-            <RouterLink :to="'/products/' + product.id">
-                <button>View Details</button>
-            </RouterLink>
+    <ProductsList :products="products" />
 
-        </div>
-    </div>
+        
 </template>
 
 
 <script>
     import {products} from '../temp-data.js'
+    import ProductsList from '../components/ProductsList.vue'
+
     export default {
         name: 'ProductsPage',
+        components: {
+            ProductsList,
+        },
         data() {
             return {
                 products,
             }
-        }
+        },
     }
 </script>
